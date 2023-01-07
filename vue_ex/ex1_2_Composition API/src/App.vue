9fe84fe108017983be61c355b458d66296b3df52
8787hyj <8787hyj@gmail.com>
2023-01-07 19:23:12 +0900

    modi

--- vue_ex/ex1_2_Composition API/src/App.vue
+++ vue_ex/ex1_2_Composition API/src/App.vue
@@ -5,36 +5,8 @@
 <script>
 // https://medium.com/arcana-network-blog/vue-3-composition-api-basics-and-patterns-44813f2c785d
 export default {
-  name: 'OptionsAPIComponent',
-  props: ['prop1'],
-  data() {
-    return {
-      logicOneVar: '',
-      logicTwoVar: 0,
-      logicThreeVar: false,
-    }
-  },
-  computed: {
-    logicFourVar() {
-      return `${this.prop} is a prop1 value`;
-    }
-  },
-
-  mounted() {
-    this.logicOneVar = 'Hello Vue 2';
-  },
-  methods: {
-    incrementLogicTwoVar() {
-      this.logicTwoVar++;
-    },
-    decrementLogicTwoVar() {
-      this.logicTwoVar--;
-    }
-  },
-  watch: {
-    logicTwoVar() {
-      this.logicThreeVar = !this.logicThreeVar;
-    }
-  }
+  name: 'CompositionAPIComponent',
+  
+  
 }
 </script>
