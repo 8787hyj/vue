0519a6049dd5aef388fd6ec06a430501860d4ce1
8787hyj <8787hyj@gmail.com>
2023-02-12 16:24:54 +0900

    Initial commit

---
+++ vue_ex/ex5_3_script_setup_v-model/my-vue-app/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
+
+const address = reactive({
+  name: "",
+  addressLine: "",
+  streetNumber: "",
+  town: "",
+  country: "",
+  postcode: "",
+  phoneNumber: "",
+  homeAddress: false,
+});
+</script>
+
+<template>
+  <div>
+    <HelloWorld v-model:name="address.name" v-model:addressLine="address.addressLine"
+      v-model:streetNumber="address.streetNumber" v-model:town="address.town" v-model:country="address.country"
+      v-model:postcode="address.postcode" v-model:phoneNumber="address.phoneNumber"
+      v-model:homeAddress="address.homeAddress" />
+    <hr />
+    <pre :style="{ 'text-align': 'left' }">{{
+  JSON.stringify(address, null, 2)
+    }}</pre>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vue_ex/ex5_3_script_setup_v-model/my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,124 @@
+<!-- HelloWorld.vue -->
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: {
+      type: String,
+      default: ""
+    },
+    addressLine: {
+      type: String,
+      default: ""
+    },
+    streetNumber: {
+      type: String,
+      default: ""
+    },
+    town: {
+      type: String,
+      default: ""
+    },
+    country: {
+      type: String,
+      default: ""
+    },
+    postcode: {
+      type: String,
+      default: ""
+    },
+    phoneNumber: {
+      type: String,
+      default: ""
+    }, 
+    homeAddress: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+const count = ref(0)
+</script>
+
+<template>
+	<section class="address">
+		<div class="address__field">
+			<label for="name">Full name</label>
+			<input
+        type="text"
+        id="name"
+        :value="name"
+        @input="$emit('update:name', $event.target.value)"
+      />
+    </div>
+			<div class="address__field">
+				<label for="address-line">Address Line 1</label>
+				<input
+        type="text"
+        id="address-line"
+        :value="addressLine"
+        @input="$emit('update:addressLine', $event.target.value)"
+      />
+  </div>
+				<div class="address__field">
+					<label for="street-house-number">Street and house #</label>
+					<input
+        type="text"
+        id="street-house-number"
+        :value="streetNumber"
+        @input="$emit('update:streetNumber', $event.target.value)"
+      />
+    </div>
+					<div class="address__field">
+						<label for="town-city">Town/City</label>
+						<input
+        type="text"
+        id="town-city"
+        :value="town"
+        @input="$emit('update:town', $event.target.value)"
+      />
+    </div>
+						<div class="address__field">
+							<label for="country">Country</label>
+							<input
+        type="text"
+        id="country"
+        :value="country"
+        @input="$emit('update:country', $event.target.value)"
+      />
+    </div>
+							<div class="address__field">
+								<label for="postcode">Postcode</label>
+								<input
+        type="text"
+        id="postcode"
+        :value="postcode"
+        @input="$emit('update:postcode', $event.target.value)"
+      />
+    </div>
+								<div class="address__field">
+									<label for="phone-number">Phone number</label>
+									<input
+        type="text"
+        id="phone-number"
+        :value="phoneNumber"
+        @input="$emit('update:phoneNumber', $event.target.value)"
+      />
+    </div>
+									<div class="address__field">
+										<label for="is-home-address">Is home address?</label>
+										<input
+        type="checkbox"
+        id="is-home-address"
+        :checked="homeAddress"
+        @change="$emit('update:homeAddress', $event.target.checked)"
+      />
+    </div>
+	</section>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
